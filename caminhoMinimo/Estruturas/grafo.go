f147c063fa644947cb56d4fd5a251c8ec70bd09b
stauffenger <lucas.miranda96@hotmail.com>
2019-11-23 15:20:06 -0300

    Criação das estruturas de mapa de cidades e do grafo de busca para o caminho mínimo, com os métodos de inicialização e impressão.

---
+++ caminhoMinimo/Estruturas/grafo.go
@@ -0,0 +1,22 @@
+package Estruturas
+
+type Grafo struct {
+	Vertice
+	valorAcumulado int
+}
+
+func NovoGrafo(nome string) *Grafo {
+	vertice := Vertice{nome: nome, ligacoes: make(map[string]Aresta)}
+	grafo := Grafo{Vertice: vertice, valorAcumulado: 0}
+	return &grafo
+}
+
+func (grafo Grafo) AdicionarLigacao(nomeFilho string, filho Grafo, distancia int, valorAcumulado int) {
+	caminho := Aresta{filho, distancia}
+	grafo.ligacoes[nomeFilho] = caminho
+	grafo.valorAcumulado = valorAcumulado
+}
+
+func (grafo Grafo) Imprimir() {
+	//Não implementado. Te juro.
+}
---
+++ caminhoMinimo/Estruturas/mapa.go
@@ -0,0 +1,25 @@
+package Estruturas
+
+type Vertice struct {
+	nome     string
+	ligacoes map[string]Aresta
+}
+
+type Aresta struct {
+	cidade    interface{}
+	distancia int
+}
+
+func NovaCidade(nome string) *Vertice {
+	cidade := Vertice{nome: nome, ligacoes: make(map[string]Aresta)}
+	return &cidade
+}
+
+func (cidade Vertice) AdicionarLigacao(nomeCidadeVizinha string, cidadeVizinha Vertice, distancia int) {
+	caminho := Aresta{cidadeVizinha, distancia}
+	cidade.ligacoes[nomeCidadeVizinha] = caminho
+}
+
+func (cidade Vertice) Imprimir() {
+	//Não implementado. Te juro.
+}
